1e13b2e39e57ee64b23f95fa62dde9addc522d91
Laura <113705334+visespaimafemeilor@users.noreply.github.com>
2023-05-25 12:20:29 +0300

    .

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main/mecanisme.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main/mecanisme.java
@@ -8,16 +8,31 @@ import com.qualcomm.robotcore.hardware.Servo;
 public class mecanisme{
 
     DcMotor slide;
-    Servo grip, pivot, swing;
+    Servo grip, pivot, turn, sula;
 
+    double Gripper_OPEN = 1;
+    double Gripper_CLOSE = 0.5;
+    double Pivot_UP = 0.2;
+    double Pivot_DOWN = 0.3;
+
+    double Pivot_SusDeTot = 0.8;
+    double Turn_FRONT = 0.5;
+    double Turn_RIGHT = 0.4;
+    double Turn_LEFT = 0.6;
+
+    int pos = 0;
+    int LOW  = -410;
+    int MID = -820;
+    int HIGH = -1155;
 
     public mecanisme(HardwareMap hardwareMap){
         slide = hardwareMap.get(DcMotor.class, "slide");
         grip = hardwareMap.get(Servo.class, "grip");
         pivot = hardwareMap.get(Servo.class, "pivot");
-        swing = hardwareMap.get(Servo.class,"swing");
+        turn = hardwareMap.get(Servo.class,"swing");
+        sula = hardwareMap.get(Servo.class,"sula");
 
-        slide.setDirection(DcMotorSimple.Direction.FORWARD);
+        slide.setDirection(DcMotorSimple.Direction.REVERSE);
         slide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         slide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         slide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -29,5 +44,12 @@ public class mecanisme{
         slide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
     }
 
+    public void gripper() {
+        if (grip.getPosition() >= Gripper_OPEN) {
+            grip.setPosition(Gripper_CLOSE);
+        } else {
+            grip.setPosition(Gripper_OPEN);
+        }
+    }
 
 }
